2a9c9c785d2d4c72fc638283fbf987bee9fc4827
Shey Barpagga <sheybarpagga@gmail.com>
2023-03-24 15:10:58 -0700

    added the other 2 models

---
+++ CRM-Team/Models/RenterService.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRM_Team.Models
+{
+    public class RenterService
+    {
+        [Key]
+        [Required]
+        [ForeignKey("Service_ID")]
+        public Guid Service_ID { get; set; }
+
+        public virtual Service Service { get; set; }
+
+        [Key]
+        [Required]
+        [ForeignKey("Renter_ID")]
+        public Guid Renter_ID { get; set; }
+
+        public string Date { get; set; }
+        public float Actual_Hours { get; set; }
+    }
+}
