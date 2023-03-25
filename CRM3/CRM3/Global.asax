9fd7189faa89946ba209b53ac577e65fb94e8b45
mingli <greenvideo6809@gmail.com>
2023-03-24 18:34:51 -0700

    3

--- CRM3/CRM3/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRM3.MvcApplication" Language="C#" %>
--- CRM3/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[AssetType] (
-    [AssetTypeId]  INT  IDENTITY (1, 1) NOT NULL,
-    [Name] VARCHAR(50) NOT NULL,
-    [AvailableNumber]    INT  NOT NULL,
-    [ServiceId]   INT  NOT NULL,
-    PRIMARY KEY CLUSTERED ([AssetTypeId] ASC),
-   
-);
