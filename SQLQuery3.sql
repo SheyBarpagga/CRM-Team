255902efef89bc5863c74878480eeef23aeda906
mingli <greenvideo6809@gmail.com>
2023-03-24 18:40:12 -0700

    5

---
+++ SQLQuery3.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Renter] (
+    [RenterId]      INT           IDENTITY (1, 1) NOT NULL,
+    [Name]             VARCHAR (50)  NOT NULL,
+    [Age]              INT           NOT NULL,
+    [Gender]           VARCHAR (50)  NOT NULL,
+    [Address]          VARCHAR (50)  NOT NULL,
+    [ContactingNumber] VARCHAR (50)  NOT NULL,
+    [EmergencyContactingNumber] VARCHAR (50)  NOT NULL,
+    [Familydoctor]     VARCHAR(50)   NOT NULL,
+    [SharingInfo]      VARCHAR (50)  NOT NULL,
+    [Income]           FLOAT (53)    NOT NULL,
+    [Employer]         VARCHAR (100) NOT NULL,
+    
+    PRIMARY KEY CLUSTERED ([RenterId] ASC),
+    
+);
---
+++ SQLQuery4.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Asset] (
+    [AssetId]     INT          IDENTITY (1, 1) NOT NULL,
+    [Description]  VARCHAR (50) NOT NULL,
+    [AssetTypeId]  INT          NOT NULL,
+    [RenterId]       INT          NOT NULL,
+    [PlateNumber]  VARCHAR(50)    NULL,
+    [SuiteNo]      VARCHAR(50)    NULL
+    PRIMARY KEY CLUSTERED ([AssetId] ASC),
+    FOREIGN KEY ([AssetTypeId]) REFERENCES [dbo].[AssetType] ([AssetTypeId]),
+    FOREIGN KEY ([RenterId]) REFERENCES [dbo].[Renter] ([RenterId]),
+);
