e2769c82c9af20b07cfb47dbae0581f1291beb2f
Stan Hung <antshung@gmail.com>
2023-03-24 20:19:02 -0700

    AssetMaintenance.cs and Invoice.cs models complete

---
+++ CRM-Team/Models/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM_Team.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public Guid InvoiceId { get; set; }
+
+        [Required]
+        public Guid ClientId { get; set; }
+
+        [Required]
+        public double BalanceDue { get; set; }
+
+        [Required]
+        public double BalancePaid { get; set;}
+
+        public int? ServiceId { get; set; }
+
+        public double? ServiceCost { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateDue { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DatePaid { get; set; }
+
+    }
+}
